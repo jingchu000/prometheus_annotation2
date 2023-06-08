d1e84fa7eb388b018d6cd08ae4e074a0e6f3c149
jingchu <jingchu@lccomputing.com>
2023-06-08 16:31:42 +0800

    feat:add annotation

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -219,16 +219,21 @@ func (c *flagConfig) setFeatureListOptions(logger log.Logger) error {
 }
 
 func main() {
+	//检查环境变量 "DEBUG" 是否存在，如果存在则设置 Go 项目的 BlockProfileRate 和 MutexProfileFraction 两个参数，
+	//以提高代码的性能和 profile 能力。
 	if os.Getenv("DEBUG") != "" {
 		runtime.SetBlockProfileRate(20)
 		runtime.SetMutexProfileFraction(20)
 	}
 
 	var (
+		// 定义两个变量 oldFlagRetentionDuration 和 newFlagRetentionDuration，
+		// 分别表示之前定义的用于保存指标数据的过期时间 (oldFlagRetentionDuration) 和
+		// 新定义的过期时间 (newFlagRetentionDuration)。
 		oldFlagRetentionDuration model.Duration
 		newFlagRetentionDuration model.Duration
 	)
-
+	// 创建一个名为 cfg 的 flagConfig 对象，其中包含用于监控的 notifier、web 和 promlog 选项。
 	cfg := flagConfig{
 		notifier: notifier.Options{
 			Registerer: prometheus.DefaultRegisterer,
@@ -239,19 +244,43 @@ func main() {
 		},
 		promlogConfig: promlog.Config{},
 	}
-
+	// 创建一个名为 a 的 Kingpin 命令对象，并使用 filepath.Base(os.Args[0]) 获取命令行程序的绝对路径，
+	// 以及 "The Prometheus monitoring server" 作为命令名称。
 	a := kingpin.New(filepath.Base(os.Args[0]), "The Prometheus monitoring server").UsageWriter(os.Stdout)
 
+	// 使用 a.Version 方法打印出程序的版本信息，
+	// 以及使用 a.HelpFlag.Short('h') 方法打印出程序的帮助信息。
 	a.Version(version.Print(appName))
-
 	a.HelpFlag.Short('h')
 
+	/*
+		使用 a.Flag 方法定义并设置 Prometheus 配置文件路径 (cfg.configFile)
+		和 Web 监听地址 (cfg.web.listenAddress)。
+	*/
 	a.Flag("config.file", "Prometheus configuration file path.").
 		Default("prometheus.yml").StringVar(&cfg.configFile)
 
 	a.Flag("web.listen-address", "Address to listen on for UI, API, and telemetry.").
 		Default("0.0.0.0:9090").StringVar(&cfg.web.ListenAddress)
 
+	/*
+		--config.file：指定 Prometheus 配置文件的路径。默认值为 prometheus.yml。
+		--web.listen-address：指定 UI、API 和遥测监听的地址。默认值为 0.0.0.0:9090。
+		--web.config.file：指定可以启用 TLS 或身份验证的配置文件的路径。这是一个实验性功能。默认值为空。
+		--web.read-timeout：指定读取请求的超时时间，并关闭空闲连接。默认值为 5m。
+		--web.max-connections：指定最大同时连接数。默认值为 512。
+		--web.external-url：指定 Prometheus 外部可访问的 URL（例如，如果 Prometheus 是通过反向代理提供服务的）。用于生成相对和绝对链接，指向 Prometheus 本身。如果 URL 有路径部分，它将用于前缀所有由 Prometheus 提供服务的 HTTP 端点。如果省略，则相关的 URL 组件将自动派生。
+		--web.route-prefix：指定 Web 端点内部路由的前缀。默认为 --web.external-url 的路径。
+		--web.user-assets：指定静态资产目录的路径，可在 /user 下访问。默认值为空。
+		--web.enable-lifecycle：启用通过 HTTP 请求进行关闭和重新加载。默认值为 false。
+		--web.enable-admin-api：启用管理员控制操作的 API 端点。默认值为 false。
+		--web.enable-remote-write-receiver：启用接受远程写入请求的 API 端点。默认值为 false。
+		--web.console.templates：指定控制台模板目录的路径，可在 /consoles 下访问。默认值为 consoles。
+		--web.console.libraries：指定控制台库目录的路径。默认值为 console_libraries。
+		--web.page-title：指定 Prometheus 实例的文档标题。默认值为 “Prometheus Time Series Collection and Processing Server”。
+		--web.cors.origin：CORS 源的正则表达式。它是完全锚定的。例如：‘https?://(domain1|domain2).com’。默认值为 .*。
+		这些命令行参数可以在启动 Prometheus 服务器时指定，以覆盖默认配置。
+	*/
 	webConfig := a.Flag(
 		"web.config.file",
 		"[EXPERIMENTAL] Path to configuration file that can enable TLS or authentication.",
@@ -296,6 +325,19 @@ func main() {
 	a.Flag("web.cors.origin", `Regex for CORS origin. It is fully anchored. Example: 'https?://(domain1|domain2)\.com'`).
 		Default(".*").StringVar(&cfg.corsRegexString)
 
+	/*
+		这段代码定义了用于配置 Prometheus 存储桶 (storage) 的参数。这些参数用于指定 Prometheus 存储桶中数据存储的基路径、数据块最小持久化时间、数据块最大持久化时间、数据块最大片段大小、wal 片段大小以及数据保留时间。
+		具体来说，这些参数包括:
+		"storage.tsdb.path" 指定了用于存储 Prometheus 数据基路径。该路径将以 data/ 开头，用于存储 Prometheus 存储桶中的数据。
+		"storage.tsdb.min-block-duration" 指定了数据块最小持久化时间，该时间将以秒为单位进行设置。此参数仅在测试环境中使用。
+		"storage.tsdb.max-block-duration" 指定了数据块最大持久化时间，该时间将以秒为单位进行设置。此参数仅在测试环境中使用。默认情况下，此参数的值为 10%。
+		"storage.tsdb.max-block-chunk-segment-size" 指定了数据块中单个片段的最大大小，单位为兆字节。
+		"storage.tsdb.wal-segment-size" 指定了 tsdb wal 片段文件的最大大小，单位为兆字节。
+		"storage.tsdb.retention" 指定了数据保留时间，该时间将以秒为单位进行设置。此参数已过期，应使用 "storage.tsdb.retention.time" 替代。
+		"storage.tsdb.retention.time" 指定了数据保留时间，该时间将以秒为单位进行设置。当此参数被设置时，它将覆盖 "storage.tsdb.retention" 参数。如果未设置此参数、"storage.tsdb.retention" 或 "storage.tsdb.retention.size",则默认保留时间为 14 天。支持的单位有年、周、天、小时、分钟、秒和毫秒。
+		"storage.tsdb.no-lockfile" 指定了是否在数据目录中创建锁文件。默认为 false。
+		这些参数可以被用于配置 Prometheus 存储桶的参数，以允许在存储桶中存储更多数据，并提高数据持久性。
+	*/
 	serverOnlyFlag(a, "storage.tsdb.path", "Base path for metrics storage.").
 		Default("data/").StringVar(&cfg.serverStoragePath)
 
@@ -327,6 +369,20 @@ func main() {
 		Default("false").BoolVar(&cfg.tsdb.NoLockfile)
 
 	// TODO: Remove in Prometheus 3.0.
+	/*
+		这段代码定义了一些命令行标志，用于配置存储选项。它使用了两个函数：serverOnlyFlag 和 agentOnlyFlag，这两个函数分别用于定义仅服务器和仅代理的标志。
+		storage.tsdb.allow-overlapping-blocks 的标志，它已被弃用，现在默认启用重叠块。该标志的默认值为 true，并且被隐藏。它的值存储在变量 b 中。
+		storage.tsdb.wal-compression，它用于压缩 tsdb WAL。它的默认值为 true，并且被隐藏。它的值存储在变量 cfg.tsdb.WALCompression 中。
+		storage.tsdb.head-chunks-write-queue-size：用于定义通过 head chunks 写入磁盘以进行 m-mapping 的队列的大小。默认值为 0，表示完全禁用队列。此标志为实验性质。它的值存储在变量 cfg.tsdb.HeadChunksWriteQueueSize 中。
+		storage.tsdb.samples-per-chunk：用于定义每个块的目标样本数。默认值为 120，并且被隐藏。它的值存储在变量 cfg.tsdb.SamplesPerChunk 中。
+		storage.agent.path：用于定义度量存储的基本路径。默认值为 data-agent/。它的值存储在变量 cfg.agentStoragePath 中。
+		storage.agent.wal-segment-size：用于定义拆分 WAL 段文件的大小。例如：100MB。它被隐藏，并使用占位符 <bytes>。它的值存储在变量 cfg.agent.WALSegmentSize 中。
+		storage.agent.wal-compression：用于压缩代理 WAL。默认值为 true。它的值存储在变量 cfg.agent.WALCompression 中。
+		storage.agent.wal-truncate-frequency：用于定义截断 WAL 并删除旧数据的频率。它被隐藏，并使用占位符 <duration>。它的值存储在变量 cfg.agent.TruncateFrequency 中。
+		storage.agent.retention.min-time：用于定义样本在被考虑删除时的最小年龄（当 WAL 被截断时）。它的值存储在变量 cfg.agent.MinWALTime 中。
+		storage.agent.retention.max-time：用于定义样本在被强制删除时的最大年龄（当 WAL 被截断时）。它的值存储在变量 cfg.agent.MaxWALTime 中。
+		storage.agent.no-lockfile：用于指定是否在数据目录中创建锁文件。默认值为 false。它的值存储在变量 cfg.agent.NoLockfile 中。
+	*/
 	var b bool
 	serverOnlyFlag(a, "storage.tsdb.allow-overlapping-blocks", "[DEPRECATED] This flag has no effect. Overlapping blocks are enabled by default now.").
 		Default("true").Hidden().BoolVar(&b)
@@ -365,6 +421,18 @@ func main() {
 	agentOnlyFlag(a, "storage.agent.no-lockfile", "Do not create lockfile in data directory.").
 		Default("false").BoolVar(&cfg.agent.NoLockfile)
 
+	/*
+		storage.remote.flush-deadline 的标志，用于指定在关闭或重新加载配置时刷新样本的等待时间。默认值为 1m，并使用占位符 <duration>。它的值存储在变量 cfg.RemoteFlushDeadline 中。
+		storage.remote.read-sample-limit，它用于定义通过远程读取接口返回的单个查询中的最大样本总数。默认值为 5e7，0 表示无限制。对于流式响应类型，此限制将被忽略。它的值存储在变量 cfg.web.RemoteReadSampleLimit 中。
+		storage.remote.read-concurrent-limit：用于定义最大并发远程读取调用数。默认值为 10，0 表示无限制。它的值存储在变量 cfg.web.RemoteReadConcurrencyLimit 中。
+		storage.remote.read-max-bytes-in-frame：用于定义在编组之前流式远程读取响应类型的单个帧中的最大字节数。请注意，客户端也可能对帧大小有限制。默认值为 1048576（1MB），这是 protobuf 推荐的默认值。它的值存储在变量 cfg.web.RemoteReadBytesInFrame 中。
+		rules.alert.for-outage-tolerance：用于定义在恢复警报的 “for” 状态时可以容忍 prometheus 中断的最长时间。默认值为 1h。它的值存储在变量 cfg.outageTolerance 中。
+		rules.alert.for-grace-period：用于定义警报和恢复 “for” 状态之间的最短持续时间。仅对配置了大于宽限期的 “for” 时间的警报进行维护。默认值为 10m。它的值存储在变量 cfg.forGracePeriod 中。
+		rules.alert.resend-delay：用于定义在向 Alertmanager 重新发送警报之前等待的最短时间。默认值为 1m。它的值存储在变量 cfg.resendDelay 中。
+		scrape.adjust-timestamps：用于调整抓取时间戳，以便将它们与预期的时间表对齐，最多可调整 scrape.timestamp-tolerance。有关更多上下文，请参见 https://github.com/prometheus/prometheus/issues/7846 。这是实验性质的。此标志将在未来版本中删除。它被隐藏，并且默认值为 true。它的值存储在变量 scrape.AlignScrapeTimestamps 中。
+		scrape.timestamp-tolerance：时间戳容差。有关更多上下文，请参见 https://github.com/prometheus/prometheus/issues/7846 。这是实验性质的。此标志将在未来版本中删除。它被隐藏，并且默认值为 2ms。它的值存储在变量 scrape.ScrapeTimestampTolerance 中。
+		alertmanager.notification-queue-capacity：用于定义待处理 Alertmanager 通知队列的容量。默认值为 10000。它的值存储在变量 cfg.notifier.QueueCapacity 中。
+	*/
 	a.Flag("storage.remote.flush-deadline", "How long to wait flushing sample on shutdown or config reload.").
 		Default("1m").PlaceHolder("<duration>").SetValue(&cfg.RemoteFlushDeadline)
 
@@ -415,7 +483,16 @@ func main() {
 
 	a.Flag("enable-feature", "Comma separated feature names to enable. Valid options: agent, exemplar-storage, expand-external-labels, memory-snapshot-on-shutdown, promql-at-modifier, promql-negative-offset, promql-per-step-stats, remote-write-receiver (DEPRECATED), extra-scrape-metrics, new-service-discovery-manager, auto-gomaxprocs, no-default-scrape-port, native-histograms. See https://prometheus.io/docs/prometheus/latest/feature_flags/ for more details.").
 		Default("").StringsVar(&cfg.featureList)
-
+	/*
+		使用 promlogflag.AddFlags 函数向命令行应用程序添加了日志记录标志。它使用了变量 cfg.promlogConfig 来配置日志记录选项。
+		接下来，定义了一个名为 write-documentation 的标志，用于生成命令行文档。此标志仅供内部使用，并且被隐藏。当此标志被指定时，它将调用 documentcli.GenerateMarkdown 函数来生成文档，并将其输出到标准输出。如果发生错误，它将退出程序并返回错误。
+		然后，代码解析命令行参数。如果发生错误，它将在标准错误上打印错误消息，并显示用法信息，然后退出程序。
+		接下来，代码使用 promlog.New 函数创建了一个新的日志记录器。它使用了变量 cfg.promlogConfig 来配置日志记录选项。
+		然后，代码调用 cfg.setFeatureListOptions 函数来设置功能列表选项。如果发生错误，它将在标准错误上打印错误消息并退出程序。
+		接下来，代码检查是否在代理模式下运行，并且是否指定了仅服务器标志。如果是，则在标准错误上打印错误消息并退出程序。
+		然后，代码检查是否在非代理模式下运行，并且是否指定了仅代理标志。如果是，则在标准错误上打印错误消息并退出程序。
+		最后，代码根据当前模式设置本地存储路径。如果处于代理模式，则使用变量 cfg.agentStoragePath；否则，使用变量 cfg.serverStoragePath。
+	*/
 	promlogflag.AddFlags(a, &cfg.promlogConfig)
 
 	a.Flag("write-documentation", "Generate command line documentation. Internal use.").Hidden().Action(func(ctx *kingpin.ParseContext) error {
@@ -455,7 +532,14 @@ func main() {
 	if agentMode {
 		localStoragePath = cfg.agentStoragePath
 	}
-
+	/*
+		computeExternalURL 函数来计算外部 URL。它使用了变量 cfg.prometheusURL 和 cfg.web.ListenAddress 来计算 URL。如果发生错误，它将在标准错误上打印错误消息并退出程序。
+			computeExternalURL 函数用于计算 Prometheus 服务器的外部 URL。它接受两个参数：prometheusURL 和 listenAddress。
+			prometheusURL 参数是一个字符串，表示 Prometheus 服务器的外部 URL。如果此参数为空，则将使用 listenAddress 参数来计算外部 URL。
+			listenAddress 参数是一个字符串，表示 Prometheus 服务器的监听地址。它用于计算外部 URL，当 prometheusURL 参数为空时。
+		接下来，代码调用 compileCORSRegexString 函数来编译 CORS 正则表达式。它使用了变量 cfg.corsRegexString 来编译正则表达式。如果发生错误，它将在标准错误上打印错误消息并退出程序。
+		最后，代码检查是否指定了 alertmanager.timeout 标志。如果是，则在日志记录器上打印警告消息，指出该标志没有效果，并将在未来删除。
+	*/
 	cfg.web.ExternalURL, err = computeExternalURL(cfg.prometheusURL, cfg.web.ListenAddress)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("parse external URL %q: %w", cfg.prometheusURL, err))
@@ -473,6 +557,12 @@ func main() {
 	}
 
 	// Throw error for invalid config before starting other components.
+	/*
+		代码使用 config.LoadFile 函数加载配置文件。它使用了变量 cfg.configFile 来指定配置文件的路径，以及变量 agentMode 来指定是否处于代理模式。如果发生错误，它将在日志记录器上打印错误消息并退出程序。
+		接下来，代码使用 cfgFile.GetScrapeConfigs 函数检查抓取配置是否有效。如果发生错误，它将在日志记录器上打印错误消息并退出程序。
+		然后，代码检查是否启用了示例存储。如果是，则检查配置文件中是否存在 ExemplarsConfig 配置。如果不存在，则使用默认配置。然后，它将变量 cfg.tsdb.MaxExemplars 设置为配置文件中指定的最大示例数。
+		最后，代码检查配置文件中是否存在 TSDBConfig 配置。如果存在，则将变量 cfg.tsdb.OutOfOrderTimeWindow 设置为配置文件中指定的值。
+	*/
 	var cfgFile *config.Config
 	if cfgFile, err = config.LoadFile(cfg.configFile, agentMode, false, log.NewNopLogger()); err != nil {
 		absPath, pathErr := filepath.Abs(cfg.configFile)
@@ -506,6 +596,17 @@ func main() {
 	// but if we don't do it now, the metrics will stay at zero until the
 	// startup procedure is complete, which might take long enough to
 	// trigger alerts about an invalid config.
+	/*
+		configSuccess 和 configSuccessTime 变量分别用于设置配置成功标志和配置成功时间。
+		cfg.web.ReadTimeout 变量用于设置 Web 读取超时。它的值从 cfg.webTimeout 变量中获取。
+		接下来，代码检查是否指定了 cfg.web.RoutePrefix 变量。如果未指定，则将其设置为 cfg.web.ExternalURL.Path 变量的值。然后，它确保 cfg.web.RoutePrefix 变量的值至少为 /。
+		接下来，代码检查是否处于代理模式。如果不是，则执行以下操作：
+			检查是否指定了旧的保留持续时间标志。如果是，则在日志记录器上打印警告消息，并将 cfg.tsdb.RetentionDuration 变量设置为旧标志的值。
+			检查是否指定了新的保留持续时间标志。如果是，则将 cfg.tsdb.RetentionDuration 变量设置为新标志的值。
+			如果未指定保留持续时间或最大字节数，则将 cfg.tsdb.RetentionDuration 变量设置为默认保留持续时间，并在日志记录器上打印信息消息。
+			检查保留持续时间是否溢出。如果是，则将其限制为 100 年，并在日志记录器上打印警告消息。
+			检查是否指定了最大块大小。如果未指定，则计算最大块大小，并将 cfg.tsdb.MaxBlockDuration 变量设置为计算出的值。
+	*/
 	configSuccess.Set(1)
 	configSuccessTime.SetToCurrentTime()
 
@@ -558,7 +659,17 @@ func main() {
 			cfg.tsdb.MaxBlockDuration = maxBlockDuration
 		}
 	}
-
+	/*
+		noStepSubqueryInterval 变量，用于存储无步长子查询间隔。它使用了 config.DefaultGlobalConfig.EvaluationInterval 变量来设置初始值。
+		接下来，代码使用 klog.ClampLevel 和 klogv2.ClampLevel 函数将 k8s 客户端的日志级别限制在 6 以下。这样，k8s 客户端就不会以明文形式记录承载令牌。
+		然后，代码使用 klog.SetLogger 和 klogv2.SetLogger 函数设置 k8s 客户端的日志记录器。它使用了带有 component 标签的日志记录器。
+		接下来，代码根据当前模式设置 modeAppName 和 mode 变量的值。如果处于代理模式，则将它们分别设置为 Prometheus Agent 和 agent；否则，将它们分别设置为 Prometheus Server 和 server。
+		然后，在日志记录器上打印启动消息，并显示当前模式和版本信息。
+		接下来，代码检查当前架构是否为 64 位。如果不是，则在日志记录器上打印警告消息，建议切换到 64 位 Prometheus 二进制文件。
+		然后，在日志记录器上打印构建上下文、主机详细信息、文件描述符限制和虚拟内存限制信息。
+		接下来，代码创建了一些变量，包括本地存储、抓取管理器、远程存储和扇出存储。它们用于管理存储和抓取操作。
+		最后，代码创建了一些上下文变量和发现管理器变量。它们用于管理 Web、规则、抓取和通知操作。
+	*/
 	noStepSubqueryInterval := &safePromQLNoStepSubqueryInterval{}
 	noStepSubqueryInterval.Set(config.DefaultGlobalConfig.EvaluationInterval)
 
@@ -604,6 +715,15 @@ func main() {
 		discoveryManagerNotify  discoveryManager
 	)
 
+	/*
+		代码检查是否启用了新的服务发现管理器。如果启用，则使用 discovery.NewManager 函数创建新的发现管理器；否则，使用 legacymanager.NewManager 函数创建旧的发现管理器。
+		然后，代码创建了一些变量，包括抓取管理器和追踪管理器。它们用于管理抓取和追踪操作。
+		接下来，代码检查是否启用了自动 GOMAXPROCS。如果启用，则使用 maxprocs.Set 函数自动设置 GOMAXPROCS。如果发生错误，则在日志记录器上打印警告消息。
+		然后，代码检查是否处于代理模式。如果不是，则执行以下操作：
+		创建一个 opts 变量，用于存储查询引擎选项。
+		使用 promql.NewEngine 函数创建一个新的查询引擎。
+		使用 rules.NewManager 函数创建一个新的规则管理器。
+	*/
 	if cfg.enableNewSDManager {
 		discovery.RegisterMetrics()
 		discoveryManagerScrape = discovery.NewManager(ctxScrape, log.With(logger, "component", "discovery manager scrape"), discovery.Name("scrape"))
@@ -650,15 +770,15 @@ func main() {
 		queryEngine = promql.NewEngine(opts)
 
 		ruleManager = rules.NewManager(&rules.ManagerOptions{
-			Appendable:      fanoutStorage,
-			Queryable:       localStorage,
-			QueryFunc:       rules.EngineQueryFunc(queryEngine, fanoutStorage),
-			NotifyFunc:      rules.SendAlerts(notifierManager, cfg.web.ExternalURL.String()),
-			Context:         ctxRule,
-			ExternalURL:     cfg.web.ExternalURL,
+			Appendable:      fanoutStorage,                                                   //存储器
+			Queryable:       localStorage,                                                    //本地时序数据库TSDB
+			QueryFunc:       rules.EngineQueryFunc(queryEngine, fanoutStorage),               //rules计算
+			NotifyFunc:      rules.SendAlerts(notifierManager, cfg.web.ExternalURL.String()), //告警通知
+			Context:         ctxRule,                                                         //用于控制ruleManager组件的协程
+			ExternalURL:     cfg.web.ExternalURL,                                             //通过Web对外开放的URL
 			Registerer:      prometheus.DefaultRegisterer,
 			Logger:          log.With(logger, "component", "rule manager"),
-			OutageTolerance: time.Duration(cfg.outageTolerance),
+			OutageTolerance: time.Duration(cfg.outageTolerance), //当prometheus重启时，保持alert状态（https://ganeshvernekar.com/gsoc-2018/persist-for-state/）
 			ForGracePeriod:  time.Duration(cfg.forGracePeriod),
 			ResendDelay:     time.Duration(cfg.resendDelay),
 		})
@@ -680,7 +800,7 @@ func main() {
 	cfg.web.LookbackDelta = time.Duration(cfg.lookbackDelta)
 	cfg.web.IsAgent = agentMode
 	cfg.web.AppName = modeAppName
-
+	//Web组件用于为Storage组件，queryEngine组件，scrapeManager组件， ruleManager组件和notifier 组件提供外部HTTP访问方式，也就是我们经常访问的prometheus的界面。
 	cfg.web.Version = &web.PrometheusVersion{
 		Version:   version.Version,
 		Revision:  version.Revision,
@@ -693,6 +813,13 @@ func main() {
 	cfg.web.Flags = map[string]string{}
 
 	// Exclude kingpin default flags to expose only Prometheus ones.
+	/*
+		首先，代码创建了一个 boilerplateFlags 变量，用于存储默认标志。它使用了 kingpin.New 函数来创建一个新的命令行应用程序。
+		然后，代码遍历命令行应用程序的标志。对于每个标志，它检查是否在 boilerplateFlags 变量中存在。如果存在，则跳过该标志；否则，将其添加到 cfg.web.Flags 变量中。
+		接下来，代码创建了一个 webHandler 变量，用于存储 Web 处理程序。它使用了 web.New 函数来创建 Web 处理程序。
+		然后，代码使用 conntrack.NewDialContextFunc 函数创建一个新的拨号上下文函数，并将其设置为默认传输的拨号上下文函数。这样，就可以使用 conntrack 来监视默认传输上的传出连接。
+		最后，代码创建了一个 externalURL 变量，用于存储外部 URL。它的值从 cfg.web.ExternalURL 变量中获取。
+	*/
 	boilerplateFlags := kingpin.New("", "").Version("")
 	for _, f := range a.Model().Flags {
 		if boilerplateFlags.GetFlag(f.Name) != nil {
@@ -712,7 +839,22 @@ func main() {
 
 	// This is passed to ruleManager.Update().
 	externalURL := cfg.web.ExternalURL.String()
-
+	/*
+		 reloaders 数组，用于存储重新加载器。每个重新加载器都有一个名称和一个重新加载函数。
+		例如，第一个重新加载器的名称为 db_storage，它的重新加载函数为 localStorage.ApplyConfig。当重新加载配置时，此函数将被调用以应用新的配置。
+		其他重新加载器的名称和重新加载函数分别为：
+		remote_storage：使用 remoteStorage.ApplyConfig 函数。
+		web_handler：使用 webHandler.ApplyConfig 函数。
+		query_engine：使用一个匿名函数，用于设置查询日志记录器。
+		scrape：使用 scrapeManager.ApplyConfig 函数。
+		scrape_sd：使用一个匿名函数，用于应用抓取服务发现配置。
+		notify：使用 notifierManager.ApplyConfig 函数。
+		notify_sd：使用一个匿名函数，用于应用通知服务发现配置。
+		rules：使用一个匿名函数，用于更新规则管理器。
+		tracing：使用 tracingManager.ApplyConfig 函数。
+	*/
+	//服务组件配置应用
+	//除了服务组件ruleManager用的方法是Update，其他服务组件的在匿名函数中通过各自的ApplyConfig方法，实现配置的管理。
 	reloaders := []reloader{
 		{
 			name:     "db_storage",
@@ -810,6 +952,14 @@ func main() {
 
 	// Start all components while we wait for TSDB to open but only load
 	// initial config and mark ourselves as ready after it completed.
+	/*
+		首先，代码使用 prometheus.MustRegister 函数注册了 configSuccess 和 configSuccessTime 指标。
+		接下来，代码创建了一个 dbOpen 通道，用于等待 TSDB 打开。
+		然后，代码定义了一个 closeOnce 类型，用于封装一个只能关闭一次的通道。它包含一个 C 字段，表示通道；一个 once 字段，表示同步锁；以及一个 Close 函数，用于关闭通道。
+		接下来，代码创建了一个 reloadReady 变量，用于等待服务器准备好处理重新加载。
+		然后，代码使用 webHandler.Listener 函数获取监听器。如果发生错误，则在日志记录器上打印错误消息并退出程序。
+		最后，代码使用 toolkit_web.Validate 函数验证 Web 配置文件。如果发生错误，则在日志记录器上打印错误消息并退出程序。
+	*/
 	dbOpen := make(chan struct{})
 
 	// sync.Once is used to make sure we can close the channel at different execution stages(SIGTERM or when the config is loaded).
@@ -840,6 +990,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	/*
+		使用 run.Group 类型创建了一个 g 变量，用于管理一组并发运行的进程。
+		首先，代码添加了一个终止处理程序。它监听 os.Interrupt 和 syscall.SIGTERM 信号，并在接收到信号时退出程序。
+		接下来，代码添加了一个抓取发现管理器。它使用 discoveryManagerScrape.Run 函数启动抓取发现管理器，并在退出时调用 cancelScrape 函数停止抓取发现管理器。
+		然后，代码添加了一个通知发现管理器。它使用 discoveryManagerNotify.Run 函数启动通知发现管理器，并在退出时调用 cancelNotify 函数停止通知发现管理器。
+		接下来，代码检查是否处于代理模式。如果不是，则添加一个规则管理器。它等待重新加载就绪，然后使用 ruleManager.Run 函数启动规则管理器，并在退出时调用 ruleManager.Stop 函数停止规则管理器。
+		然后，代码添加了一个抓取管理器。它等待重新加载就绪，然后使用 scrapeManager.Run 函数启动抓取管理器，并在退出时调用 scrapeManager.Stop 函数停止抓取管理器。
+		接下来，代码添加了一个追踪管理器。它等待重新加载就绪，然后使用 tracingManager.Run 函数启动追踪管理器，并在退出时调用 tracingManager.Stop 函数停止追踪管理器。
+		最后，代码添加了一个重新加载处理程序。它监听 syscall.SIGHUP 信号和 Web 处理程序的重新加载通道，并在接收到信号或消息时重新加载配置。它使用 reloadConfig 函数重新加载配置。
+	*/
 	var g run.Group
 	{
 		// Termination handler.
@@ -981,6 +1141,16 @@ func main() {
 			},
 		)
 	}
+
+	/*
+		这段代码继续使用 run.Group 类型添加更多的进程。
+		首先，代码添加了一个初始配置加载程序。它等待 TSDB 打开，然后使用 reloadConfig 函数重新加载配置。如果重新加载成功，则关闭 reloadReady 通道，并将 Web 处理程序标记为准备就绪。
+		接下来，代码检查是否处于代理模式。如果不是，则添加一个 TSDB 进程。它使用 openDBWithMetrics 函数打开本地存储，并在退出时调用 fanoutStorage.Close 函数关闭存储。
+		如果处于代理模式，则添加一个 WAL 存储进程。它使用 agent.Open 函数打开本地存储，并在退出时调用 fanoutStorage.Close 函数关闭存储。
+		然后，代码添加了一个 Web 处理程序进程。它使用 webHandler.Run 函数启动 Web 服务器，并在退出时调用 cancelWeb 函数取消 Web 上下文。
+		接下来，代码添加了一个通知器进程。它等待重新加载就绪，然后使用 notifierManager.Run 函数启动通知器管理器，并在退出时调用 notifierManager.Stop 函数停止通知器管理器。
+		最后，代码使用 g.Run 函数启动所有进程。如果发生错误，则在日志记录器上打印错误消息并退出程序。
+	*/
 	{
 		// Initial configuration loading.
 		cancel := make(chan struct{})
